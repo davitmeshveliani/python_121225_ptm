88e459b080b69f0be38408a9206ccd94a463eea3
davitmeshveliani <davitmeshveliani979@gmail.com>
2026-04-06 10:12:06 +0200

    add change damashka_25

--- DAMASHKA_25.py
+++ DAMASHKA_25.py
@@ -1,48 +1,48 @@
-def divide_numbers(x, y):
-    try:
-        x = float(x)
-        y = float(y)
-    except ValueError:
-        raise ValueError("Ошибка: Введено некорректное число.")
+# def divide_numbers(x, y):
+#     try:
+#         x = float(x)
+#         y = float(y)
+#     except ValueError:
+#         raise ValueError("Ошибка: Введено некорректное число.")
+#
+#     if y == 0:
+#         raise ZeroDivisionError("Ошибка: Деление на ноль невозможно.")
+#
+#     return x / y
+# result = divide_numbers(0, "k")
 
-    if y == 0:
-        raise ZeroDivisionError("Ошибка: Деление на ноль невозможно.")
-
-    return x / y
-result = divide_numbers(345, "k")
-
-# # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ß
-import logging
-
-logging.basicConfig(
-    level=logging.ERROR,
-    format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s"
-)
-
-def divide_numbers(x, y):
-    try:
-        x = float(x)
-        y = float(y)
-
-        if x == 0:
-            raise ZeroDivisionError("Ошибка: ни одно значение не должно быть нулем.")
-
-        if y == 0:
-            raise ZeroDivisionError("Ошибка: Деление на ноль невозможно.")
-
-        return x / y
-
-    except ValueError:
-        logging.error("Ошибка: Введено некорректное число.")
-        return
-
-    except ZeroDivisionError as e:
-        logging.error(str(e))
-        return
-
-    except Exception:
-        logging.error("Системная ошибка.")
-        return
-
-
-divide_numbers(0, "k")
+# # # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ß
+# import logging
+#
+# logging.basicConfig(
+#     level=logging.ERROR,
+#     format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s"
+# )
+#
+# def divide_numbers(x, y):
+#     try:
+#         x = float(x)
+#         y = float(y)
+#
+#         if x == 0:
+#             raise ZeroDivisionError("Ошибка: ни одно значение не должно быть нулем.")
+#
+#         if y == 0:
+#             raise ZeroDivisionError("Ошибка: Деление на ноль невозможно.")
+#
+#         return x / y
+#
+#     except ValueError:
+#         logging.error("Ошибка: Введено некорректное число.")
+#         return
+#
+#     except ZeroDivisionError as e:
+#         logging.error(str(e))
+#         return
+#
+#     except Exception:
+#         logging.error("Системная ошибка.")
+#         return
+#
+#
+# divide_numbers(0, "k")
