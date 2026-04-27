889313e99d20105d443d09d50e78578d1c9de6c4
davitmeshveliani <davitmeshveliani@gmail.com>
2026-04-27 21:47:08 +0200

    I ve refactored the code DAMASHKA_31

--- DAMASHKA_31.py
+++ DAMASHKA_31.py
@@ -1,39 +1,38 @@
-
-import re
-from typing import List
-
-def clean_tags(tag_string: str) -> List[str]:
-    """
-    Splits a string of tags into a list using various delimiters (space, comma, semicolon, slash).
-
-    Args:
-    tag_string (str): The raw string containing tags and delimiters.
-
-    Returns:
-    List[str]: A list of cleaned, non-empty tag strings.
-    """
-    tags = re.split(r'[ ,;/]+', tag_string)
-    result = [t for t in tags if t]
-    return result
-
-if __name__ == "__main__":
-    tag_old = "python,data-science / machine-learning/data-science;AI neural-network"
-    print(clean_tags(tag_old))
-
-# ~~~~~~~~~~~ 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import re
 
-def find_data(text:str) -> None:
+def find_data(text:str):
     """
     Finds and prints dates in DD/MM/YYYY, DD.MM.YYYY, or DD-MM-YYYY format.
     Args:
         text (str): The input string where dates should be located.
     """
     num_parter = r"(\d{2})[./-](\d{2})[./-](\d{4})"
-    date = re.finditer(num_parter, text)
-    for m in date:
-        print(m.group())
-if __name__ == "__main__":
-    text = ("The events N 123456 happened on 15/03/2025, 01.12.2024 "
+    yield from re.finditer(num_parter, text)
+    start_text = ("The events N 123456 happened on 15/03/2025, 01.12.2024 "
             "and 09-09-2023. Deadline: 28/02/2022.")
-    find_data(text)
+    new_gen = find_data(start_text)
+    final_result = [match.group() for match in find_data(start_text)]
+
+
+########  2   ############
+
+import re
+
+def clean_tags(tag_string: str) :
+    """
+        Yields cleaned tags from a string, splitting by various delimiters.
+
+        Args:
+            tag_string: The raw string containing tags (space, comma, semicolon, slash).
+
+        Yields:
+            str: A cleaned, non-empty tag string with whitespace stripped.
+        """
+    yield from (t.strip() for t in re.split(r'[ ,;/]+', tag_string) if t.strip())
+
+user_data = "python,data-science / machine-learning/data-science;AI neural-network"
+tag_list =  list(clean_tags(user_data))
+
+
+
+
