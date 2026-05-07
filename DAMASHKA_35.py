0c28e7aaae1a6024f539e7f52c3483a017a93631
davitmeshveliani <davitmeshveliani@gmail.com>
2026-05-07 09:00:40 +0200

    add DAMASHKA_35

--- DAMASHKA_35.py
+++ DAMASHKA_35.py
@@ -6,29 +6,25 @@ class User:
     total_users = 0
 
     def __init__(self, user_name, password):
-
-        """Initialize user and validate data."""
-
-        if not user_name:
-            raise ValueError("Invalid name")
-
-        if len(password) < 5:
-            raise ValueError(f"Invalid password: '{password}'")
+        if not user_name.strip() or len(password.strip()) < 5:
+            raise ValueError("Invalid name or password")
 
         self.user_name = user_name
         self.password = password
         User.total_users += 1
 
+
     def __str__(self):
         """Return a string representation of the user."""
         return f"User: {self.user_name}"
 
-    def get_total(self):
+    @classmethod
+    def get_total(cls):
         """Return the total number of users created."""
-        return User.total_users
+        return cls.total_users
 
 user1 = User("alice", "secret")
-user2 = User("bob", "123456")
-
-#print(f"Total users: {user1.get_total()}",user1,sep="\n")
+user2 = User("bob", "12345")
 #user3 = User("bob", "qwe")
+
+print(f"Total users: {user1.get_total()}",user1,sep="\n")
